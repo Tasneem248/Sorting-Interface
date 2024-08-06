a5b7317d9cbfa441d551d19355bea284868dcc81
TASNIM <tasnim.fattal@arafas.com>
2024-08-06 11:29:36 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('#sortForm').on('submit', function(event) {
+        event.preventDefault();
+        let numbers = $('#numbers').val().split(',').map(Number);
+        $.ajax({
+            url: 'http://localhost:7280/api/sort',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(numbers),
+            success: function(response) {
+                $('#resultsTableBody').empty();
+                for (let algorithm in response) {
+                    let row = `<tr>
+                        <td>${algorithm}</td>
+                        <td>${response[algorithm].toFixed(2)}</td>
+                    </tr>`;
+                    $('#resultsTableBody').append(row);
+                }
+            },
+            error: function(error) {
+                console.log(error);
+                alert('An error occurred: ' + error.responseText);
+            }
+        });
+    });
+});
